01a41c2d10333493d614b1dc5ddf4218f5430a0a
Alex-Korir <alexkogo63@gmail.com>
2023-04-06 23:08:19 +0300

    initial commit

---
+++ src/Components/validation.js
@@ -0,0 +1,6 @@
+export default function Validation(){
+    const errors = {}
+
+    const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,6}$/;
+    const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z]
+}   
